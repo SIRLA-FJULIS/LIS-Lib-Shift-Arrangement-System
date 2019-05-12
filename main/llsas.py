258b16e73f0f54d10e1b13089f0645c696874b74
Sam Yang <samabc75@gmail.com>
2019-05-12 16:13:26 +0800

    加了點簡單的樣板

---
+++ main/llsas.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "llsas"
+db_path = os.path.dirname(__file__)
+db_path = os.path.join(db_path, 'llsas.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////{}".format(db_path)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 減少記憶體使用
+
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html", name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+    
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/name_form', methods=['GET', 'POST'])
+def name_form():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+
+            if app.config['MAIL_ADMIN']:
+                send_email(app.config['MAIL_ADMIN'], 'New User', 'mail/new_user', user=user)
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('name_form'))
+    return render_template("name_form.html", form=form, name=session.get('name'), known=session.get('known', False))
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['MAIL_SUBJECT_PREFIX'] + subject, sender = app.config['MAIL_SENDER'], recipients=[to])
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
