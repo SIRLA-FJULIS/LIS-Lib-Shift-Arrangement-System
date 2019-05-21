a46599e6918490d36a362074ca20c1932c9a3a2a
Arashi <opp40519@gmail.com>
2019-05-21 18:08:07 +0800

    sign up

--- app/llsas.py
+++ app/llsas.py
@@ -31,7 +31,11 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template("500.html"), 500
-
+"""
+@app.route('/')
+def index():
+    return render_template("sign_up.html")
+"""
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
