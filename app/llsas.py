55b3d64d76140f1666f554b846ed727563c03cda
bessyhuang <bessy110576@gmail.com>
2019-05-22 16:36:56 +0800

    adjust forgot_pwd (html, route, wtf)

--- app/llsas.py
+++ app/llsas.py
@@ -3,7 +3,8 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "llsas"
@@ -19,6 +20,11 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password")
     submit = SubmitField("Submit")
 
+class ForgotPasswordForm(FlaskForm):
+    account = StringField("Account", validators = [DataRequired()])
+    email = EmailField("Email", validators = [DataRequired(), Email()])
+    submit = SubmitField("Submit")
+    
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -37,9 +43,17 @@ def login():
 def signup():
     return render_template("sign_up.html")
 
-@app.route('/forgotpwd')
-def forgetpwd():
-    return render_template("forgot_pwd.html")
+@app.route('/forgotpassword', methods = ['GET', 'POST'])
+def forgotpassword():
+    account = None
+    email = None
+    form = ForgotPasswordForm()
+    if form.validate_on_submit():
+        account = form.account.data
+        form.account.data = ''
+        email = form.email.data
+        form.email.data = ''
+    return render_template('forgot_pwd.html', form = form, account = account, email = email)
 
 @app.errorhandler(404)
 def page_not_found(e):
