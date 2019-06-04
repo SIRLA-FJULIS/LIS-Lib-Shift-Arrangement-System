5e82afcf0d5ee2adfbff7d58bef5151610094e73
bessyhuang <bessy110576@gmail.com>
2019-06-04 15:14:03 +0800

    build llsas.db & create testing account (404040123, ej03xu35k3) & modify def __repr__(self):

--- app/llsas.py
+++ app/llsas.py
@@ -8,14 +8,37 @@ from wtforms.validators import DataRequired, Email, EqualTo
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "llsas"
-db_path = os.path.dirname(__file__)
-db_path = os.path.join(db_path, 'llsas.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////{}".format(db_path)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'llsas.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 減少記憶體使用
-
 db = SQLAlchemy(app)
 
 # ------------------- class --------------------
+class UserDataTable(db.Model):
+    __tablename__ = 'userData'
+    userID = db.Column(db.Integer, primary_key = True, unique = True, index = True)
+    userName = db.Column(db.String(64), index = True)
+    userPassword = db.Column(db.String)
+    userEmail = db.Column(db.String, unique = True)
+    userRole = db.Column(db.String, index = True)
+    arrangements = db.relationship('ShiftArrangementTable', backref = 'user', lazy = 'dynamic')
+
+    def __repr__(self):
+        return '<User (%r:%r)>' % (self.userID, self.userName)
+
+class ShiftArrangementTable(db.Model):
+    __tablename__ = 'shiftArrangement'
+    arrangementID = db.Column(db.Integer, primary_key = True, unique = True, index = True)
+    arrangementDate = db.Column(db.Date, index = True)
+    arrangementPeriod = db.Column(db.String)
+    arrangementCheckIn = db.Column(db.DateTime, nullable = True)
+    arrangementCheckInState = db.Column(db.String(10), default = 'None')
+    arrangementCheckOut = db.Column(db.DateTime, nullable = True)
+    arrangementCheckOutState = db.Column(db.String(10), default = 'None')
+    uID = db.Column(db.Integer, db.ForeignKey('userData.userID'))
+
+    def __repr__(self):
+        return '<Arrangement (%r:%r)>' % (self.arrangementID, self.arrangementDate)
 
 class LoginForm(FlaskForm):
     account = StringField("學號", validators = [DataRequired()])
@@ -52,32 +75,6 @@ class CheckinStatus(FlaskForm):
     time= DateField('刷卡時間', format='%Y:%m:%d')
     submit_in = SubmitField("簽到")
     submit_out = SubmitField("簽退")
-
-class UserDataTable(db.Model):
-    __tablename__ = 'userData'
-    userID = db.Column(db.Integer, primary_key = True, unique = True, index = True)
-    userName = db.Column(db.String(64), index = True)
-    userPassword = db.Column(db.String)
-    userEmail = db.Column(db.String, unique = True)
-    userRole = db.Column(db.String, index = True)
-    arrangements = db.relationship('ShiftArrangementTable', backref = 'user', lazy = 'dynamic')
-
-    def __repr__(self):
-        return '<User %r:%r>' % self.userID, self.userName
-
-class ShiftArrangementTable(db.Model):
-    __tablename__ = 'shiftArrangement'
-    arrangementID = db.Column(db.Integer, primary_key = True, unique = True, index = True)
-    arrangementDate = db.Column(db.Date, index = True)
-    arrangementPeriod = db.Column(db.String)
-    arrangementCheckIn = db.Column(db.DateTime, nullable = True)
-    arrangementCheckInState = db.Column(db.String(10), default = 'None')
-    arrangementCheckOut = db.Column(db.DateTime, nullable = True)
-    arrangementCheckOutState = db.Column(db.String(10), default = 'None')
-    uID = db.Column(db.Integer, db.ForeignKey('userData.userID'))
-
-    def __repr__(self):
-        return '<Arrangement %r:%r>' % self.asdf, self.arrangementDate
 
 # ------------------- route --------------------
 
