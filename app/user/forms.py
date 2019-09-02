22ee92fd0152b713b84e2f9d334ce130e371c048
Sam Yang <samabc75@gmail.com>
2019-09-02 14:34:43 +0800

    刪除原本很醜的預約

--- app/user/forms.py
+++ app/user/forms.py
@@ -1,15 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, SubmitField, StringField, TextAreaField
+from wtforms import SubmitField, StringField, TextAreaField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired
-
-class BookForm(FlaskForm):
-    time1 =BooleanField('8:30-10:00')
-    time2 =BooleanField('10:00-12:00')
-    time3 =BooleanField('12:00-13:00')
-    time4 =BooleanField('13:30-15:30')
-    time5 =BooleanField('15:30-17:30')
-    submit = SubmitField("送出")
 
 class ContactForm(FlaskForm):
     topic = StringField("主旨", validators = [DataRequired()])
