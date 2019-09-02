db6db806e4b3366be76c69f64242a6c996002a2a
Sam Yang <samabc75@gmail.com>
2019-09-02 16:06:06 +0800

    預約日曆雛形

--- app/user/views.py
+++ app/user/views.py
@@ -1,8 +1,8 @@
 from flask import render_template
-from app.user.forms import BookForm
 from app.user.forms import ContactForm
-from app.user.forms import BookForm, ContactForm
 from app.user import bp
+from calendar import Calendar
+from datetime import date
 
 @bp.route('/dashboard')
 def dashboard():
@@ -10,8 +10,10 @@ def dashboard():
 
 @bp.route('/book', methods = ['GET', 'POST'])
 def book():
-    form = BookForm()
-    return render_template('user/book.html', form = form)
+    cal = Calendar(0)
+    year = date.today().year
+    cal_list = [cal.monthdatescalendar(year, i+1) for i in range(12)]
+    return render_template('user/book.html', year=year, cal=cal_list)
 
 @bp.route('/contact', methods = ['GET', 'POST'])
 def contact():
