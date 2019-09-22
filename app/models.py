6b5667b6f87f3f9f2a2079bbeb44305b6ea3a815
Sam Yang <samabc75@gmail.com>
2019-09-22 19:37:25 +0800

    modify table column name again

--- app/models.py
+++ app/models.py
@@ -2,14 +2,14 @@ from . import db
 
 class Table_UserData(db.Model):
     __tablename__ = 'userData'
-    userID = db.Column(db.Integer, primary_key = True, unique = True, index = True)
-    userName = db.Column(db.String(64), index = True)
-    userPassword = db.Column(db.String)
-    userEmail = db.Column(db.String, unique = True)
-    userRole = db.Column(db.String, index = True)
-    ID_for_arrangements = db.relationship('Table_ShiftArrangement', backref = 'user', lazy = 'dynamic')
-    ID_for_modification = db.relationship('Table_ModifyApplication', backref = 'user', lazy = 'dynamic')
-    ID_for_contact = db.relationship('Table_Contact', backref = 'user', lazy = 'dynamic')
+    id = db.Column(db.Integer, primary_key = True, unique = True, index = True)
+    name = db.Column(db.String(64), index = True)
+    password = db.Column(db.String)
+    email = db.Column(db.String, unique = True)
+    role = db.Column(db.String, index = True)
+    id_for_arrangements = db.relationship('Table_ShiftArrangement', backref = 'user', lazy = 'dynamic')
+    id_for_modification = db.relationship('Table_ModifyApplication', backref = 'user', lazy = 'dynamic')
+    id_for_contact = db.relationship('Table_Contact', backref = 'user', lazy = 'dynamic')
     def __repr__(self):
         return '<User %r:%r>' % (self.userID, self.userName)
 
@@ -22,8 +22,8 @@ class Table_ShiftArrangement(db.Model):
     checkInState = db.Column(db.String(10), default = 'None')
     checkOut = db.Column(db.DateTime, nullable = True)
     checkOutState = db.Column(db.String(10), default = 'None')
-    uid = db.Column(db.Integer, db.ForeignKey('Table_UserData.userID'))
-    did = db.Column(db.Integer, db.ForeignKey('Table_Duty.dutyID'))
+    uid = db.Column(db.Integer, db.ForeignKey('Table_UserData.id'))
+    did = db.Column(db.Integer, db.ForeignKey('Table_Duty.id'))
     modification = db.relationship('Table_ModifyApplication', backref = 'arrangement', lazy = 'dynamic')
     def __repr__(self):
         return '<Arrangement %r : %r>' % (self.uID, self.arrangementDate)
@@ -34,29 +34,31 @@ class Table_ModifyApplication(db.Model):
     Apply to modify the shift arrangement.
     """
     __tablename__ = 'modifyApplication'
-    modifyDate = db.Column(db.Date)
-    modifyReason = db.Column(db.String)
-    uID = db.Column(db.Integer, db.ForeignKey('Table_UserData.userID'))
-    aID = db.Column(db.Integer, db.ForeignKey('Table_ShiftArrangement.arrangementID'))
-    dID = db.Column(db.Integer, db.ForeignKey('Table_Duty.dutyID'))
+    id = db.Column(db.Integer, primary_key = True, unique = True, index = True)
+    date = db.Column(db.Date)
+    reason = db.Column(db.String)
+    uid = db.Column(db.Integer, db.ForeignKey('Table_UserData.id'))
+    aid = db.Column(db.Integer, db.ForeignKey('Table_ShiftArrangement.id'))
+    did = db.Column(db.Integer, db.ForeignKey('Table_Duty.id'))
     def __repr__(self):
         return '<Modification %r : %r - %r>' % (self.uID, self.modifyDate, self.modifyPeriod)
 
 
 class Table_News(db.Model):
     __tablename__ = 'news'
-    newsTitle = db.Column(db.String)
-    newsDateTime = db.Column(db.DateTime)
-    newsContent = db.Column(db.Text)
+    id = db.Column(db.Integer, primary_key = True, unique = True, index = True)
+    title = db.Column(db.String)
+    dateTime = db.Column(db.DateTime)
+    content = db.Column(db.Text)
     def __repr__(self):
         return '<News %r [%r]>' % (self.newsTitle, self.newsDateTime)
 
 class Table_Duty(db.Model):
     __tablename__ = 'duty'
-    dutyID = db.Column(db.Integer, primary_key = True, unique = True, index = True)
-    dutyPeriod = db.Column(db.String)
-    dutyContent = db.Column(db.String)
-    dutyExplanation = db.Column(db.Text)
+    id = db.Column(db.Integer, primary_key = True, unique = True, index = True)
+    period = db.Column(db.String)
+    content = db.Column(db.String)
+    explanation = db.Column(db.Text)
     modification = db.relationship('Table_ModifyApplication', backref = 'duty', lazy = 'dynamic')
     arrangement = db.relationship('Table_ShiftArrangement', backref = 'duty', lazy = 'dynamic')
     def __repr__(self):
@@ -64,9 +66,9 @@ class Table_Duty(db.Model):
 
 class Table_Contact(db.Model):
     __tablename__ = 'contact'
-    contactDate = db.Column(db.Date)
-    contactEmail = db.Column(db.String)
-    contactContent = db.Column(db.Text)
-    uID = db.Column(db.Integer, db.ForeignKey('Table_UserData.userID'))
+    date = db.Column(db.Date)
+    email = db.Column(db.String)
+    content = db.Column(db.Text)
+    uid = db.Column(db.Integer, db.ForeignKey('Table_UserData.id'))
     def __repr__(self):
         return '<Contact %r [%r]' % (self.contactDate, self.contactEmail)
