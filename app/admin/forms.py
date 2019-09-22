fb843029be8172a488ddc6ef04d9d5047529232d
Chensm <a8icecst@gmail.com>
2019-09-22 20:26:37 +0800

    add duty management

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import DateField, SubmitField, StringField, TextAreaField
+from wtforms import DateField, SubmitField, StringField, TextAreaField, SelectField
 from datetime import datetime
 
 class CheckInOutForm(FlaskForm):
@@ -12,3 +12,9 @@ class NewsForm(FlaskForm):
     post_time = DateField('發布時間', format='%Y-%m-%d', default=datetime.today())
     content = TextAreaField("內容")
     submit = SubmitField("確定")
+
+class WorkingContentForm(FlaskForm):
+	period = SelectField("時段",choices=[('時段1','8:00 ~ 10:00'),('時段2','10:00 ~ 12:00'), ('時段3','12:00 ~ 13:30'), ('時段4','13:30 ~ 15:30'), ('時段4','15:30 ~ 17:30')])
+	working_content = StringField("工作內容")
+	description = TextAreaField("工作內容說明")
+	submit = SubmitField("確定")
--- app/admin/views.py
+++ app/admin/views.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from app.admin.forms import CheckInOutForm, NewsForm
+from app.admin.forms import CheckInOutForm, NewsForm, WorkingContentForm
 from app.admin import bp
 
 @bp.route('/checkinout', methods = ['GET', 'POST'])
@@ -28,3 +28,14 @@ def add_news():
 def news():
     form = NewsForm()
     return render_template('admin/news.html', form = form)
+
+@bp.route('/working_content_management', methods = ['GET', 'POST'])
+def working_content_management():
+    form = WorkingContentForm()
+    return render_template('admin/working_content_management.html', form = form)
+
+@bp.route('/change_working_content', methods = ['GET', 'POST'])
+def change_working_content():
+    form = WorkingContentForm()
+    return render_template('admin/change_working_content.html', form = form)
+
