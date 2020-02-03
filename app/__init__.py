0070778e0b0c277dc3c3d8591bc9286b9dfb7911
Sam Yang <samabc75@gmail.com>
2020-02-03 17:41:50 +0800

    make migration and database work

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import config
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 login_manager = LoginManager()
--- llsas.py
+++ llsas.py
@@ -1,6 +1,7 @@
 import os
 from app import create_app, db
-from flask_migrate import Migrate
+from flask_migrate import Migrate, upgrade
+from app.models import *
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
