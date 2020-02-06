5e1bf0446a36460faed7b734583668ad9e468dac
nightcatv <egoism.no.sakura@gmail.com>
2020-02-06 18:06:48 +0800

    adjust: (auth/views.py) use flask-login

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, session, flash
+from flask import render_template, redirect, url_for, session, flash, request
 from flask_login import login_user
 from app.auth.forms import LoginForm, ForgotPasswordForm, ChangePasswordForm
 from app.auth import bp
@@ -9,23 +9,30 @@ def login():
     account = None
     password = None
     form = LoginForm()
+
     if form.validate_on_submit():
         account = form.account.data
         password = form.password.data
         user = UserData.query.filter_by(id = account).first()
         form.account.data = ''
+        
         if user is not None and user.verify_password(password):
             session['logged_in'] = True
-            session['username'] = user.name
+            session['user_name'] = user.name
             session['user_id'] = user.id
+            login_user(user)
+            next = request.args.get('next')
+            
             if user.role == 'admin':
                 session['role'] = 'Admin'
-                return redirect(url_for('admin.dashboard'))
+                next = url_for('admin.dashboard')
             else:
                 session['role'] = 'User'
-                return redirect(url_for('user.dashboard'))
+                next = url_for('user.dashboard')
+                
+            return redirect(next)
         else:
-            flash('Invalid username or password')
+            flash('Invalid account or password')
             return redirect(url_for('auth.login'))
     return render_template('auth/login.html', form = form)
 
