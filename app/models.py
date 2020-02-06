c8fb01a3f74a10c8d1e04b2ce37b46e743de90b0
nightcatv <egoism.no.sakura@gmail.com>
2020-02-06 20:02:39 +0800

    add: (Feature) Role and Permission

--- app/models.py
+++ app/models.py
@@ -6,16 +6,63 @@ from flask_login import UserMixin
 def load_user(user_id):
     return UserData.query.get(int(user_id))
 
+class Permission:
+    ADMIN = 1
+    USER = 0
+    UNKNOWN = -1
+
+class Role(db.Model):
+    __tablename__ = 'role'
+    id = db.Column(db.Integer, primary_key = True, index = True)
+    name = db.Column(db.String(64), unique = True, index = True)
+    default = db.Column(db.Boolean, default = False, index = True)
+    permissions = db.Column(db.Integer)
+    
+    users = db.relationship('UserData', backref='role')
+
+    def set_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions = perm
+
+    def has_permission(self, perm):
+        return self.permissions & perm == perm
+    
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'Admin': Permission.ADMIN,
+            'User': Permission.USER,
+            'Unknown': Permission.UNKNOWN
+        }
+        default_role = 'User'
+        for r in roles:
+            role = Role.query.filter_by(name = r).first()
+            if role is None:
+                role = Role(name = r)
+                role.set_permission(roles[r])
+            role.default = (role.name == default_role)
+            db.session.add(role)
+        db.session.commit()
+
+    def __init__(self, **kwargs):
+        super(Role, self).__init__(**kwargs)
+        if self.permissions is None:
+            self.permissions = -1
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
 class UserData(UserMixin, db.Model):
     __tablename__ = 'userData'
     id = db.Column(db.Integer, primary_key = True, unique = True, index = True)
     name = db.Column(db.String(64), index = True)
     password_hash = db.Column(db.String(128))
     email = db.Column(db.String, unique = True, index = True)
-    role = db.Column(db.String, index = True)
-    id_for_arrangements = db.relationship('ShiftArrangement', backref = 'user', lazy = 'dynamic')
-    id_for_modification = db.relationship('ModifyApplication', backref = 'user', lazy = 'dynamic')
-    id_for_contact = db.relationship('Contact', backref = 'user', lazy = 'dynamic')
+    
+    role_ref = db.Column(db.Integer, db.ForeignKey('role.id'), index = True)
+    arrangements_ref = db.relationship('ShiftArrangement', backref = 'user', lazy = 'dynamic')
+    modification_ref = db.relationship('ModifyApplication', backref = 'user', lazy = 'dynamic')
+    contact_ref = db.relationship('Contact', backref = 'user', lazy = 'dynamic')
     
     @property
     def password(self):
@@ -27,6 +74,17 @@ class UserData(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def can(self, perm):
+        return self.role_ref is not None and self.role_ref.has_permission(perm)
+
+    def is_administrator(self):
+        return self.can(Permission.ADMIN)
+
+    def __init__(self, **kwargs):
+        super(UserData, self).__init__(**kwargs)
+        if self.role_ref is None:
+            self.role_ref = Role.query.filter_by(default = True).first()
 
     def __repr__(self):
         return '<User %r:%r>' % (self.id, self.name)
