db82b0b38c0d06b642adeb1e3d32f00cbf829aef
Sam Yang <samabc75@gmail.com>
2020-02-10 07:12:01 +0800

    delete handmade session, using flask login to manage session

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, url_for, session, flash, request
-from flask_login import login_user
+from flask_login import login_user, login_required, logout_user
 from app.auth.forms import LoginForm, ForgotPasswordForm, ChangePasswordForm
 from app.auth import bp
 from app.models import UserData
@@ -17,9 +17,6 @@ def login():
         form.account.data = ''
         
         if user is not None and user.verify_password(password):
-            session['logged_in'] = True
-            session['user_name'] = user.name
-            session['user_id'] = user.id
             login_user(user)
             next = request.args.get('next')
             
@@ -49,9 +46,9 @@ def forgotpassword():
     return render_template('auth/forgot_pwd.html', form = form, account = account, email = email)
 
 @bp.route('/logout')
-#@login_required
+@login_required
 def logout():
-    session.pop('logged_in')
+    logout_user()
     return redirect(url_for('main.index'))
 
 @bp.route('/changepassword', methods=['GET', 'POST'])
