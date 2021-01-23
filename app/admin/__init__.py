590c12bcd4abae103754b9d817c31da60149dcf2
Arashi <opp40519@gmail.com>
2021-01-23 17:02:06 +0800

    test

--- app/admin/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('admin', __name__)
-
-from . import views
--- app/main/views.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: UTF-8 -*-
-from flask import render_template
-from app.main import bp
-
-@bp.route('/')
-def index():
-   return render_template("main/index.html")
